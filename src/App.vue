bba09bd876419f09589c4d52e76708531206b191
felipe <feleteli@egresados.ubiobio.cl>
2020-08-17 17:41:10 -0400

    vue keycloak integration login

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+      <v-container>
+        <v-row justify="center">
+          <v-col 
+            cols="12"
+            md="6"
+            >
+            <v-card>
+              <v-row justify="center">
+              <v-card-title>
+                Token Authorization
+              </v-card-title>
+              </v-row>
+              <v-row justify="center" class="pa-3">
+                <v-card-text>
+                  {{ authorizationToken }}
+                </v-card-text>
+              </v-row>
+            </v-card>
+          </v-col>
+          <v-col 
+          cols="12"
+          md="6">
+            <v-card>
+              <v-row justify="center">
+              <v-card-title>
+                User Information
+              </v-card-title>
+              </v-row>
+              <v-row justify="center" class="pa-3">
+                <v-card-text>
+                  {{ decodeToken }}
+                </v-card-text>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import * as jwtDecode from 'jwt-decode'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  },
+
+  data: () => ({
+    authorizationToken: '',
+    decodeToken : {},
+  }),
+  methods: {
+  },
+  mounted: async function(){
+    const getTokenPromise = function(){
+          return new Promise(resolve=>{
+          setTimeout(()=>{
+            const token = localStorage.getItem('vue-token')
+            if(token)
+            {
+              resolve(token)
+            }
+          },10)
+        })
+    }
+    this.authorizationToken = await getTokenPromise()
+    //this.authorizationToken = localStorage.getItem('vue-token')
+    this.decodeToken =  jwtDecode(this.authorizationToken)
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,46 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import * as Keycloak from 'keycloak-js'
+import vuetify from './plugins/vuetify';
 Vue.config.productionTip = false
+let initOptions = {
+  url: 'http://rc.keycloak.ledpack.cl/auth', realm: 'Demo-Realm', clientId: 'vue-test-app', onLoad:'login-required'
+}
+let keycloak = Keycloak(initOptions);
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+keycloak.init({ onLoad: initOptions.onLoad }).success((auth) =>{
+    
+  if(!auth) {
+    window.location.reload();
+  } else {
+    console.log('authenticated')
+  }
+
+  new Vue({
+    vuetify,
+    render: h => h(App)
+  }).$mount('#app')
+
+
+  localStorage.setItem("vue-token", keycloak.token);
+  localStorage.setItem("vue-refresh-token", keycloak.refreshToken);
+
+  setInterval(() =>{
+    keycloak.updateToken(30).success((refreshed)=>{
+      if (refreshed) {
+        console.log('Token refreshed '+ refreshed)
+        localStorage.setItem("vue-token",keycloak.token)
+        localStorage.setItem("vue-refresh-token",keycloak.refreshToken)
+      } else {
+        console.log('Token not refreshed, valid for '
+        + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+      }
+    }).error(()=>{
+        console.log('failed to refresh token')
+    });
+
+
+  }, 10000
+  )
+
+})
